ae7631123400f9c55a9aecccce71c2b958f2b238
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2021-08-10 19:17:26 +0100

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        //This is a constructor. We will be passing options to this class when we add it to our starter config. Where we add it to our dependancy injection container
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
