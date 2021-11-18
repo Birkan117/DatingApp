f266656cc865e0e4b272cc3078d6284eda3b8dde
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2021-11-18 11:38:14 +0000

    End of Section 5

---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,6 @@
+export interface User{
+    username: string;
+    token: string;
+}
+//Interface in typescript is different from C#.
+//we use it to specify something is a type of something
---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import {map} from 'rxjs/operators';
+import { User } from '../_models/user';
+
+//This means we can inject this service anywhere else in the application
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  //Adding the dollar is highlighting that this is a observable
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+  //Using : gives the thing before it a type so "'this': any" means this has a type of any now
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
